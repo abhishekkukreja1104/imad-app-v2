e8beec4d2e368c50833c0efd464479a889c4506c
abhishekkukreja1104 <kukrejaabhishek11@gmail.com>
2017-06-14 22:01:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,10 +3,18 @@ var button= document.getElementById('counter');
 var counter=0;
 button.onclick = function() {
     
-
-counter=counter+1;
-var span =document.getElementById('count');
+var request =new XMLHttpRequest();
+request.onreadystatechange = functio(){
+    if(request.readystate ===XMLHttpRequest.DONE){
+        if(request.status ===200){
+            var counter=requet.responseText;
+            var span =document.getElementById('count');
 span.innerHTML=counter.toString();
 
+        }
+    }
+};
+request.open("GET","https://cloud.imad.hasura.io/counter",true);
+  request.send(null);
   
 };
